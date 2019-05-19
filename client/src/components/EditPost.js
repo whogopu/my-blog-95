1a0dfdc9aa9ae5523f9f1b29344e739ece9fe960
Gopal sharma <gopal151295@gmail.com>
2019-05-19 16:10:27 +0530

    edit a post styling done

--- client/src/components/EditPost.js
+++ client/src/components/EditPost.js
@@ -43,38 +43,42 @@ class EditPost extends Component {
 		}
 
 		return (
-			<div className="card my-4 mt-5">
-				<h3 className="card-header">Edit Post</h3>
-				<div className="card-body">
-					<form onSubmit={this.onFormSubmit}>
-						<div className="form-group">
-							<label htmlFor="title">Title</label>
-							<input
-								type="text"
-								id="title"
-								className="form-control"
-								placeholder="Enter Title"
-								value={this.state.title}
-								onChange={(e) => this.setState({ title: e.target.value })}
-							/>
-						</div>
-						<div className="form-group">
-							<label htmlFor="body">Body</label>
-							<textarea
-								rows="5"
-								type="text"
-								id="body"
-								className="form-control"
-								placeholder="Enter Body"
-								value={this.state.body}
-								onChange={(e) => this.setState({ body: e.target.value })}
-							/>
-						</div>
+			<div className="container" >
+				<div className="card my-4 mt-5 shadow-sm">
+					<h3 className="card-header text-center">EDIT POST</h3>
+					<div className="card-body">
+						<form onSubmit={this.onFormSubmit}>
+							<div className="form-group">
+								<label htmlFor="title">Title</label>
+								<input
+									type="text"
+									id="title"
+									className="form-control"
+									placeholder="Enter Title"
+									value={this.state.title}
+									onChange={(e) => this.setState({ title: e.target.value })}
+								/>
+							</div>
+							<div className="form-group">
+								<label htmlFor="body">Body</label>
+								<textarea
+									rows="5"
+									type="text"
+									id="body"
+									className="form-control"
+									placeholder="Enter Body"
+									value={this.state.body}
+									onChange={(e) => this.setState({ body: e.target.value })}
+								/>
+							</div>
 
-						<button type="submit" className="btn btn-primary">
-							Submit
-						</button>
-					</form>
+							<div className="float-right">
+								<button type="submit" className="btn btn-primary">
+									Submit
+								</button>
+							</div>
+						</form>
+					</div>
 				</div>
 			</div>
 		);
